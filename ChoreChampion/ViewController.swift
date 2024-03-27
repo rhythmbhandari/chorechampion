1c586abf4f07f85438d7f5d6f07774434ec1edb3
Rhythm Bhandari <rhythmbhandari@outlook.com>
2024-03-26 20:36:18 -0400

    Initial Commit

---
+++ ChoreChampion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChoreChampion
+//
+//  Created by Rhythm Bhandari on 3/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
